20d608bd938e9e5b3d25271790d5ae70634112f4
Oliver Tzeng <olivertzeng@proton.me>
2024-03-24 20:38:20 +0800

    🐛Reorder fetches

--- fetch/fetchbase.go
+++ fetch/fetchbase.go
@@ -10,12 +10,54 @@ var Fetches = []Fetch{
 		Description: "Fetch for Linux subreddits",
 	},
 	{
-		Name:        "nofetch",
-		Description: "Smol POSIX sh fetch",
-	},
-	{
 		Name:        "neofetch",
 		Description: "Classic, bloated Bash fetch",
+	},
+	{
+		Name:		 "3dfetch",
+		Description: "Neofetch for older kids",
+	},
+	{
+		Name:		 "101fetch",
+		Description: "Le101 frfr",
+	},
+	{
+		Name:		 "afetch",
+		Description: "Another Neofetch written in C",
+	},
+	{
+		Name:		 "akkufetch",
+		Description: "Battery info fetching tool",
+	},
+	{
+		Name:		 "androfetch",
+		Description: "Neofetch but for green small minions",
+	},
+	{
+		Name:        "archfetch",
+		Description: "Is's a fetch btw",
+	},
+	{
+		Name:        "cfetch",
+		Description: "Smol fetch",
+	},
+	{
+		Name:        "cpufetch",
+		Description: "CPU architecture fetching tool",
+	},
+	{
+		Name:		 "cutefetch",
+		Description: "Neofetch but cuter",
+	},
+	{
+		Name:		 "efetch",
+		Description: "Neofetch written in C++",
+	},
+	{
+		Name:        "fastfetch",
+		Description: "Neofetch written in C",
+	},
+	{
 		Name:		 "gfetch",
 		Description: "Onefetch alternative",
 	},
@@ -68,8 +110,12 @@ var Fetches = []Fetch{
 		Description: "Neofetch with Nerdfont",
 	},
 	{
+		Name:        "nofetch",
+		Description: "Smol POSIX sh fetch",
+	},
+	{
 		Name:        "onefetch",
-		Description: "Neoetch for Git repos",
+		Description: "Neofetch for Git repos",
 	},
 	{
 		Name:        "paleofetch",
@@ -80,136 +126,16 @@ var Fetches = []Fetch{
 		Description: "Smol POSIX sh fetch",
 	},
 	{
-		Name:        "ramfetch",
-		Description: "Fetch for RAM info",
-	},
-	{
-		Name:        "ufetch",
-		Description: "Tiny fetch",
-	},
-	{
-		Name:        "nerdfetch",
-		Description: "Neofetch with Nerdfont",
-	},
-	{
-		Name:        "archfetch",
-		Description: "Is's a fetch btw",
-	},
-	{
-		Name:        "cfetch",
-		Description: "Smol fetch",
-	},
-	{
-		Name:        "onefetch",
-		Description: "Fetch for Git repos",
-	},
-	{
-		Name:        "hyfetch",
-		Description: "Neofetch with LGBTQ+ flags",
-	},
-	{
-		Name:        "uwufetch",
-		Description: "uwu",
-	},
-	{
 		Name:        "picofetch",
 		Description: "Smol fetch",
-	},
-	{
-		Name:        "macchina",
-		Description: "Fetch written in Rust™",
-	},
-	{
-		Name:        "minifetch",
-		Description: "Smol fetch",
-	},
-	{
-		Name:        "paleofetch",
-		Description: "Neofetch, but written in C",
-	},
-	{
-		Name:        "cpufetch",
-		Description: "CPU architecture fetching tool",
-	},
-	{
-		Name:        "gpufetch",
-		Description: "GPU architecture fetching tool",
-	},
-	{
-		Name:        "fastfetch",
-		Description: "Neofetch written in C",
-	},
-	{
-		Name:        "wfetch",
-		Description: "Basically W*ndows Neofetch",
-	},
-	{
-		Name:		 "hentaifetch",
-		Description: "Sussy Baka",
-	},
-	{
-		Name:		 "akkufetch",
-		Description: "Battery info fetching tool",
-	},
-	{
-		Name:		 "cutefetch",
-		Description: "Fetch but cuter",
 	},
 	{
 		Name:		 "pokefetch",
 		Description: "Neofetch for kids",
 	},
 	{
-		Name:		 "lovefetch",
-		Description: "Neofetch for couples",
-	},
-	{
-		Name:		 "3dfetch",
-		Description: "Neofetch for older kids",
-	},
-	{
-		Name:		 "101fetch",
-		Description: "Le101 frfr",
-	},
-	{
-		Name:		 "afetch",
-		Description: "Another Neofetch written in C",
-	},
-	{
-		Name:		 "info.sh",
-		Description: "Neofetch that show infos...?",
-	},
-	{
-		Name:		 "mfetch",
-		Description: "Neofetch for minimalists",
-	},
-	{
-		Name:		 "myfetch",
-		Description: "It's not my fetch though",
-	},
-	{
-		Name:		 "efetch",
-		Description: "Neofetch written in C++",
-	},
-	{
-		Name:		 "gfetch",
-		Description: "Onefetch alternative",
-	},
-	{
 		Name:		 "pwy",
 		Description: "Neofetch but for weather",
-	},
-	{
-		Name:		 "zfetch",
-		Description: "Neofetch written in zig",
-	},
-	{
-		Name:		 "qfetch",
-		Description: "Yet another Neofetch written in Rust™",
-	},
-	{
-		Name:		 "ferris-fetch",
-		Description: "Neofetch but shows Rust™ install",
 	},
 	{
 		Name:		 "pythonfetch",
@@ -220,31 +146,51 @@ var Fetches = []Fetch{
 		Description: "Neofetch but shows PHP install",
 	},
 	{
-		Name:		 "sfetch",
-		Description: "Neofetch but simpler",
+		Name:		 "qfetch",
+		Description: "Yet another Neofetch written in Rust™",
 	},
 	{
-		Name:		 "stupidfetch",
-		Description: "Neofetch without bloat",
-	},
-	{
-		Name:		 "sfetch",
-		Description: "Neofetch with only 28 lines of shell",
-	},
-	{
-		Name:		 "smfetch",
-		Description: "Another fetch written in shell",
+		Name:        "ramfetch",
+		Description: "Neoetch for RAM info",
 	},
 	{
 		Name:		 "sf",
 		Description: "Takes much less keys to get system info",
 	},
 	{
+		Name:		 "sfetch",
+		Description: "Neofetch with only 28 lines of shell",
+	},
+	{
+		Name:		 "stupidfetch",
+		Description: "Neofetch without bloat",
+	},
+	{
+		Name:		 "smfetch",
+		Description: "Another fetch written in shell",
+	},
+	{
+		Name:        "ufetch",
+		Description: "Tiny fetch",
+	},
+	{
+		Name:        "uwufetch",
+		Description: "uwu",
+	},
+	{
 		Name:		 "vfetch",
 		Description: "Sounds like fetch",
 	},
 	{
-		Name:		 "androfetch",
-		Description: "Neofetch but for green small minions",
+		Name:        "wfetch",
+		Description: "Basically W*ndows Neofetch",
+	},
+	{
+		Name:		 "xfetch",
+		Description: "Ultimate debloated Neofetch",
+	},
+	{
+		Name:		 "zfetch",
+		Description: "Neofetch written in zig",
 	},
 }
